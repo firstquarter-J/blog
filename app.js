cdf3ab78dc9b083d1392fb49e94938026c578408
SangHyun <>
2022-01-05 11:58:26 +0900

    https public folder ues

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// HTTPS 인증 폴더 ~?
+app.use(express.static('public'));
+
 const connect = require('./schemas')
 connect()
 const authMiddleware = require("./middlewares/auth-middleware")
